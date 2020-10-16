808fac26343f26e4faa0d3ec2794b62673a2edaa
Ralph L <lettau.ralph@gmail.com>
2020-10-16 19:45:22 +0200

    Strings

--- src/test/java/de/qsmq/java11introduction/StringTest.java
+++ src/test/java/de/qsmq/java11introduction/StringTest.java
@@ -6,9 +6,7 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Paths;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,33 +17,24 @@ class StringTest {
 
     @BeforeEach
     void readFile() {
-        Resource resource = new ClassPathResource("users.txt");
+        Resource resource = new ClassPathResource("string.txt");
         String content = "";
         try {
-            content =
-                    readFileAsString(resource.getFile().getAbsolutePath());
+            content = Files.readString(resource.getFile().toPath());
         } catch (IOException e) {
             e.printStackTrace();
         }
         lines = content.split("\n");
     }
 
-    //@Test
+    @Test
     void lengthOfAString() {
         String string = lines[0];
-        int length = string.trim().length();
+        int length = string.strip().length();
         assertThat(length).isEqualTo(3);
-    }
 
-
-    private String readFileAsString(String filePath) {
-        byte[] encoded = new byte[0];
-        try {
-            encoded = Files.readAllBytes(Paths.get(filePath));
-        } catch (IOException e) {
-            return "";
-        }
-        return new String(encoded, StandardCharsets.UTF_8);
+        String longString = "ABC".repeat(100);
+        assertThat(longString.strip()).hasSize(300);
     }
 
 

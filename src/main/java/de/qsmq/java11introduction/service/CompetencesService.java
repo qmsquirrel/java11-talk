fa2838502bfa98197df33d9d9ab9ce2c674d74a4
Ralph L <lettau.ralph@gmail.com>
2020-10-16 19:07:56 +0200

    Optionals

--- src/main/java/de/qsmq/java11introduction/service/CompetencesService.java
+++ src/main/java/de/qsmq/java11introduction/service/CompetencesService.java
@@ -3,7 +3,6 @@ package de.qsmq.java11introduction.service;
 import de.qsmq.java11introduction.entity.Competences;
 import de.qsmq.java11introduction.repository.MapCompetencesRepository;
 import de.qsmq.java11introduction.repository.ListCompetencesRepository;
-import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -21,24 +20,20 @@ public class CompetencesService {
     }
 
     public Competences getCompetences(int i) {
-        return mainCompetencesRepository.findById(i).get();
+        return mainCompetencesRepository.findById(i).orElseThrow();
     }
 
 
     public List<Integer> getIdsByIdentifier(String identifier) {
-        List<Competences> competences = mainCompetencesRepository.findByName(identifier).orElseThrow(NoSuchElementException::new);
+        List<Competences> competences = mainCompetencesRepository.findByName(identifier).orElseThrow();
         return competences.stream()
                 .map( t -> t.getId())
                 .collect(Collectors.toList());
     }
 
     public Competences getCompetencesInAnyRepository(int i) {
-        Optional<Competences> competences;
-        competences = mainCompetencesRepository.findById(i);
-        System.out.println(competences != null);
-        if (!competences.isPresent()) {
-            competences = additionalCompetencesRepository.findById(i);
-        }
+        Optional<Competences> competences = mainCompetencesRepository.findById(i)
+                .or(() -> additionalCompetencesRepository.findById(i));
         return competences.get();
     }
 
